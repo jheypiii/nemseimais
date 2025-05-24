03c9de4bfab90c14781acbead39a2c39ac6cf2ee
jheypiii <jvivierasouza@gmail.com>
2025-05-23 22:43:21 -0300

    corrige embedding v2

--- android/app/src/main/kotlin/com/example/provasmart/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/provasmart/MainActivity.kt
@@ -2,5 +2,6 @@ package com.jheypiii.provasmart
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterActivity() {
+    // Não é necessário nenhum código adicional
 }
